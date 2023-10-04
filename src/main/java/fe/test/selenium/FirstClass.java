bb5aae39a4b43a1600028e39d6d67b2008f0c850
https://github.com/tomaszJablonski <tjablonski1990@gmail.com>
2023-10-04 19:35:54 +0200

    add to gitigonre 1 position change main name class and sout text.

---
+++ src/main/java/fe/test/selenium/FirstClass.java
@@ -0,0 +1,7 @@
+package fe.test.selenium;
+
+public class FirstClass {
+    public static void main(String[] args) {
+        System.out.println("This is my program for automation testing");
+    }
+}
